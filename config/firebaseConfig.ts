b341bcead5bad1dd39cd4b1c428b483cf69a0eb2
TheSoftNode <thesoftnode@gmail.com>
2024-10-26 22:12:37 +0100

    add .env file to protect secrets

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyD2qUPQKC82Rdh5QaicU6C6_HJJzV7NPTY",
-    authDomain: "loanme-app.firebaseapp.com",
-    projectId: "loanme-app",
-    storageBucket: "loanme-app.appspot.com",
-    messagingSenderId: "253038793243",
-    appId: "1:253038793243:web:7b1e63d1c6698250695ebe"
-};
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
